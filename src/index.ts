2aa5e950341c963c51b2022eaead27c2ebb0c90b
Glen <Glen.Burchfield.91@gmail.com>
2020-05-07 19:34:12 -0400

    move over interfaces, the rest of User controllers

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,14 @@
 import {MongoClient, Db, ObjectId} from "mongodb";
-
-export interface User {
-    fName: string;
-    lName: string;
-    email: string;
-    phone: string;
-}
-
-interface CreateUserPayload extends User {
-    _id?: ObjectId;
-    password: string;
-}
-
-export interface UserDocInternal extends User {
-    _id: string;
-    password: string;
-}
-
-interface CreateUser {
-    (payload: CreateUserPayload) : Promise<UserDocInternal>
-}
-
-interface UsersControllers {
-    CreateUser: CreateUser
-}
+import {
+    CreateUserPayload,
+    UserDocInternal,
+    UsersControllers,
+    DefaultExport,
+    UserSpaceHolder
+} from "./interfaces";
 
 const Users = (db:Db): UsersControllers => {
+
     const CreateUser = async (payload: CreateUserPayload): Promise<UserDocInternal> => {
         const Users = db.collection('Users')
         const userScrubbed: CreateUserPayload = {
@@ -43,13 +26,49 @@ const Users = (db:Db): UsersControllers => {
         return result
     }
 
-    return {
-        CreateUser
+    const CreateUserSpaceHolder = async (email: string): Promise<UserSpaceHolder> => {
+        const UserSpaceHolders = db.collection('UserSpaceHolders')
+        const userScrubbed: UserSpaceHolder = {
+            _id: new ObjectId(),
+            email: email,
+            notJoined: true
+        }
+        const response = await UserSpaceHolders.insertOne(userScrubbed)
+        const result = response.insertedId
+        return result
     }
-}
 
-interface DefaultExport {
-    Users: UsersControllers;
+    const GetUser = async (query: {[propName:string]: any}): Promise<UserDocInternal> => {
+        const Users = db.collection('Users')
+        return await Users.findOne(query)
+    }
+
+    const GetManyUsers = async (user_ids: string []): Promise<UserDocInternal[]> => {
+        const user_obj_ids: ObjectId [] = user_ids.map(id => new ObjectId(id))
+        const Users = db.collection('Users')
+        const query = { _id: { $in: user_obj_ids } }
+        return await Users.find(query, {projection: {password: -1}}).toArray()
+    }
+
+    const GetUserByEmail = async (email: string): Promise<UserDocInternal> => {
+        const query = {email: email.toLowerCase()}
+        return GetUser(query)
+    }
+
+    const UserExists = async (email: string): Promise<boolean> => {
+        let result = true
+        let user = await GetUserByEmail(email)
+        return result && !!user
+
+    }
+
+    return {
+        CreateUser,
+        CreateUserSpaceHolder,
+        GetManyUsers,
+        GetUserByEmail,
+        UserExists
+    }
 }
 
 const CollectionHandlers = async (dbPromise: Promise<Db>): Promise<DefaultExport> => {
